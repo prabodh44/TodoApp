7e51a3073a3074195f3c091d45748f7b4e033865
ith <ith@iths-Mac-mini.local>
2017-10-27 12:01:26 +0545

    Alert when nothing is written

--- js/script.js
+++ js/script.js
@@ -2,15 +2,17 @@ $('document').ready(function(){
     //create a close button to 
     //add to each list item
     var nodeList = document.querySelectorAll('li');
-    console.log(nodeList);
     
-    for(var i = 0; i <  nodeList.length; i++){
+    for(var i = 0; i < nodeList.length; i++){
         var span = document.createElement('span');
+        $('span').addClass('close');
         var txt = document.createTextNode('\u00D7');
         span.appendChild(txt);
-        $('span').addClass('close');
+        
         nodeList[i].appendChild(span);
+        console.log('nodeList innerHTML ' + nodeList[i].innerHTML);
     }
+    $('span').addClass('close');
     
     //click on close button
     //to hide current list
@@ -30,6 +32,36 @@ $('document').ready(function(){
             event.target.classList.toggle('checked');
         }
     },false);
-
-
 });
+
+$('document').ready(function(){
+    $('button#addItem').click(function(){
+        var li = document.createElement('li');
+        var inputValue = document.getElementById('todoText').value;
+        var text = document.createTextNode(inputValue);
+        
+        if(text.length == 0){
+            alert('You must write something');
+        }else{
+            li.appendChild(text);
+            document.getElementById('list').appendChild(li);
+            document.getElementById('todoText').value = '';
+        }
+        
+        
+        var span = document.createElement("SPAN");
+        var txt = document.createTextNode("\u00D7");
+        
+        span.appendChild(txt);
+        li.appendChild(span);
+        $('li').addClass('list-group-item');
+        $('span').addClass('close');
+        var close = $('.close');
+        for(var i = 0; i < close.length + 1; i++){
+           close[i].onclick = function(){
+               var div = this.parentElement;
+               div.style.display = "none";
+           };
+        }
+    });
+})
