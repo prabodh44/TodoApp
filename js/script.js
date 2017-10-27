e45a868dfe4348ca389ac019f9de81d56a206c1b
prabodh44 <prabodhtuladhar44@gmail.com>
2017-10-27 07:44:35 +0545

    Added the scripts and close button functionality

---
+++ js/script.js
@@ -0,0 +1,26 @@
+$('document').ready(function(){
+    //create a close button to 
+    //add to each list item
+    var nodeList = document.querySelectorAll('li');
+    console.log(nodeList);
+    
+    for(var i = 0; i <  nodeList.length; i++){
+        var span = document.createElement('span');
+        var txt = document.createTextNode('\u00D7');
+        span.appendChild(txt);
+        $('span').addClass('close');
+        nodeList[i].appendChild(span);
+    }
+    
+    //click on close button
+    //to hide current list
+    var close = $('.close');
+    for(var i = 0; i < close.length; i++){
+       close[i].onclick = function(){
+           var div = this.parentElement;
+           div.style.display = "none";
+           
+       };
+    }
+    console.log(close.length);
+});
